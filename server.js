fe4e251d4401ede65c6f0652fd162ea426c47ca5
Alex-Nav <alejugger@gmail.com>
2022-06-13 10:04:17 -0500

    finishing basic CRUD

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
+    // ========================
+    // Middlewares
+    // ========================
+app.set('view engine', 'ejs') // so we can creat html with ejs
+app.use(bodyParser.urlencoded({ extended: true })) // parsing middleware. takes data from the form and and adds the to the body
+app.use(bodyParser.json()) // makes the objects we send from the backend to the server a JSON   
+app.use(express.static('public')) // makes that express can read the folder with the main js and css
 
 let connectionString = 'mongodb+srv://mongo:esterno7@cluster0.jkuqp.mongodb.net/?retryWrites=true&w=majority'
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
+MongoClient.connect(connectionString, { useUnifiedTopology: true }) // conection to the srver
     .then(client => {
         console.log('Connected to Database')
 
-        const db = client.db('star-wars-quotes')
-        const quotesCollection = db.collection('quotes')
-
+        const db = client.db('star-wars-quotes') //naming our database
+        const quotesCollection = db.collection('quotes') // naming our collection for this program
+            // ========================
+            // Routes
+            // ========================
         app.listen(3000, function() {
             console.log("funcionando en ell 3000");
         })
@@ -22,7 +28,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
             quotesCollection.insertOne(req.body)
                 .then(result => {
                     console.log(result)
-                    res.redirect('/')
+                    res.redirect('/') // refresshes the page after inserting is finisehd
                 })
                 .catch(error => console.error(error))
         })
@@ -33,5 +39,32 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                 })
                 .catch( /* ... */ )
         })
+        app.put('/quotes', (req, res) => {
+            console.log(req.body)
+            quotesCollection.findOneAndUpdate({ name: 'yoda' }, {
+                    $set: {
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                }, {
+                    upsert: true
+                })
+                .then(result => {
+                    console.log(result);
+                    res.json('Success');
+                })
+                .catch(error => console.error(error))
+        })
+        app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne({ name: req.body.name })
+                .then(result => {
+                    if (result.deletedCount === 0) {
+                        return res.json('No quote to delete')
+                    }
+                    res.json('Deleted Darth Vadar\'s quote')
+                })
+                .catch(error => console.error(error))
+        })
+
     })
     //mongodb+srv://mongo:esterno7@cluster0.jkuqp.mongodb.net/?retryWrites=true&w=majority
