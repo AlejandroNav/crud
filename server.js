dce8766e22b6a7041ad110b015c19c5c80d95563
Alex-Nav <alejugger@gmail.com>
2022-06-13 01:03:14 -0500

    creating and reading steps

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function() {
-    console.log("funcionando en ell 3000");
-})
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
-console.log('si funciona el server');
+let connectionString = 'mongodb+srv://mongo:esterno7@cluster0.jkuqp.mongodb.net/?retryWrites=true&w=majority'
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+
+        const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+
+        app.listen(3000, function() {
+            console.log("funcionando en ell 3000");
+        })
+
+        app.post('/quotes', (req, res) => { //CREATE
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.get('/', (req, res) => { // READ 
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch( /* ... */ )
+        })
+    })
+    //mongodb+srv://mongo:esterno7@cluster0.jkuqp.mongodb.net/?retryWrites=true&w=majority
